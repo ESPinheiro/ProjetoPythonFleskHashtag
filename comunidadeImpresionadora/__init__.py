1403430ca15f851c994662a71c7bccad12dc3170
Pinheiro <50429324+ESPinheiro@users.noreply.github.com>
2023-02-15 22:47:08 -0300

    Update __init__.py

--- comunidadeImpresionadora/__init__.py
+++ comunidadeImpresionadora/__init__.py
@@ -2,13 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'de0e6b061d4b66b963098cd53bca94e6'
-
+if os.getenv('DATABASE_URL'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+else:
 # configuração para criação do banco de dados
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
