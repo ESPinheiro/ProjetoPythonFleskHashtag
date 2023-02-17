bf68645877b0d3adfccbad427d52061ff88bdf5a
Pinheiro <50429324+ESPinheiro@users.noreply.github.com>
2023-02-16 22:17:54 -0300

    Update __init__.py

--- comunidadeImpresionadora/__init__.py
+++ comunidadeImpresionadora/__init__.py
@@ -1,9 +1,10 @@
+import sqlalchemy
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
-
+import SQLAlchemy
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'de0e6b061d4b66b963098cd53bca94e6'
@@ -18,6 +19,13 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
 
-
-
+from comunidadeImpresionadora import models
+engine = sqlalchemy.create_engine( app.config['SQLALCHEMY_DATABASE_URI'])
+if not engine.has_table("usuario"):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print('Base de Dados criado')
+else:
+    print('Base de Dados já existente')
 from comunidadeImpresionadora import route
